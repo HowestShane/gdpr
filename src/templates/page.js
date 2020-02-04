8859aea3280ee40e04ea60044d25bb850496bc73
ShaneHowest <shane.deconinck@howest.be>
2020-02-04 09:06:59 +0100

    form

--- src/templates/page.js
+++ src/templates/page.js
@@ -21,12 +21,22 @@ export default class Page extends React.Component {
                     <section className={'wrapper '}>
                         <div className="inner">
                             <h2>Inschrijven</h2>
+                            <p>
+                                Langs deze weg kan u uw interesse doorgeven. U ontvangt dan een betalingsuitnodiging waarna uw inschrijving definitief is.
+                            </p>
                             <form method="POST" data-netlify="true" name={_.get(this.props, 'pageContext.name')}>
                                 <label>Naam<br />
                                     <input type='text' name='Naam' />
                                 </label>
+                                {_.get(this.props, 'pageContext.name') === 'gdpr-actuele-stand-van-zaken' && 
+                                    <label><input type="checkbox" name="8 juni" value="8 juni" /> 8 juni</label><br/>
+                                    <label><input type="checkbox" name="9 juni" value="9 juni" /> 9 juni</label>
+                                }
                                 <label>E-mailadres<br />
                                     <input type='text' name='Naam' />
+                                </label>
+                                <label>Opmerkingen<br />
+                                    <textarea name='Opmerkingen'></textarea>
                                 </label>
                                 <input type='submit' value='Verzenden'/>
                             </form>
